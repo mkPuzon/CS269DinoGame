5414f6f377ed77bae265ad9f45011d4d455d2ebf
racerman100 <osthom25@colby.edu>
2023-01-25 11:11:50 -0500

    nothing

--- title_screen.py
+++ title_screen.py
@@ -113,7 +113,7 @@ class Title(State):
         if self.two_unlocked == False:
             self.level_two.draw_button(display,False)
         
-
+    
     
     def check_to_unlock(self):
         pass
